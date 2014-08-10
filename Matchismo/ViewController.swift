8c3ce1605a577ed6898997ef21d4ab2262149a2d
Karol <karol.s.baginski@gmail.com>
2014-08-10 16:04:03 +0100

    Initial Commit

---
+++ Matchismo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Matchismo
+//
+//  Created by Karol Baginski on 10/08/2014.
+//  Copyright (c) 2014 Karol Baginski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
