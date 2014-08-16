2aa80cb925396ef9cb22150f43584fdc18f5905e
Karol <karol.s.baginski@gmail.com>
2014-08-16 12:59:04 +0100

    Initial commit

---
+++ Matchismo/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Matchismo
+//
+//  Created by Karol Baginski on 10/08/2014.
+//  Copyright (c) 2014 Karol Baginski. All rights reserved.
+//
+
+import Foundation
---
+++ Matchismo/Model/Deck.swift
@@ -0,0 +1,9 @@
+//
+//  Deck.swift
+//  Matchismo
+//
+//  Created by Karol Baginski on 11/08/2014.
+//  Copyright (c) 2014 Karol Baginski. All rights reserved.
+//
+
+import Foundation
