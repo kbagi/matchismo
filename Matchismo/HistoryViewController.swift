ff0460b097f136b2559df5543ad828bc35b5bcae
Karol <karol.s.baginski@gmail.com>
2014-08-17 21:19:39 +0100

    Change handing of status messages (applied to Cards so far) 70% of assignment 3 done

---
+++ Matchismo/HistoryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryViewController.swift
+//  Matchismo
+//
+//  Created by Karol Baginski on 17/08/2014.
+//  Copyright (c) 2014 Karol Baginski. All rights reserved.
+//
+
+import Foundation
---
+++ Matchismo/Model/SetFindingGame.swift
@@ -0,0 +1,9 @@
+//
+//  SetFindingGame.swift
+//  Matchismo
+//
+//  Created by Karol Baginski on 16/08/2014.
+//  Copyright (c) 2014 Karol Baginski. All rights reserved.
+//
+
+import Foundation
